6c81dd1e975f16361326edc2c624054a0c0fafc5
Drinnn <pedro_lontro@hotmail.com>
2022-07-25 16:54:40 -0300

    feat(auth): add jwt cookie on signup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { default as signUpRouter } from "./routes/sign-up";
 import { default as signInRouter } from "./routes/sign-in";
@@ -11,7 +12,14 @@ import errorHandler from "./middlewares/error-handler";
 import { RouteNotFoundError } from "./errors/route-not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(signUpRouter);
 app.use(signInRouter);
