11bf834494d8d2ff855c103469fb9a67db5ba019
Drinnn <pedro_lontro@hotmail.com>
2022-07-26 08:54:33 -0300

    feat(auth): add logic to signout

--- auth/src/routes/sign-out.ts
+++ auth/src/routes/sign-out.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/api/auth/signOut", (req, res) => {});
+router.post("/api/auth/signOut", (req, res) => {
+  req.session = null;
+
+  res.status(200).send({});
+});
 
 export default router;
