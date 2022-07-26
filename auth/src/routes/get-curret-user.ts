8782de07ca704c45a64c13660c8592d0a2a129dd
Drinnn <pedro_lontro@hotmail.com>
2022-07-26 08:52:31 -0300

    feat(auth): add logic to getcurrentuser

--- auth/src/routes/get-curret-user.ts
+++ auth/src/routes/get-curret-user.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.get("/api/auth", (req, res) => {});
+router.get("/api/auth", (req, res) => {
+  if (!req.session || !req.session.jwt) {
+    return res.status(200).send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_SECRET!);
+    res.status(200).send({ currentUser: payload });
+  } catch (err) {
+    res.status(401).send({ currentUser: null });
+  }
+});
 
 export default router;
