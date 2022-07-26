8bd306c917349306236a69dba60a0399bf1f6dbc
Drinnn <pedro_lontro@hotmail.com>
2022-07-26 08:38:49 -0300

    feat(auth): add logic to signin

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,7 +1,50 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
+import User from "../models/user";
+import { Password } from "../services/password";
 
 const router = Router();
 
-router.post("/api/auth/signIn", (req, res) => {});
+router.post(
+  "/api/auth/signIn",
+  [
+    body("email").isEmail().withMessage("E-mail must be valid"),
+    body("password").trim().notEmpty().withMessage("Password must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const isPasswordValid = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!isPasswordValid) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_SECRET!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    return res.status(200);
+  }
+);
 
 export default router;
