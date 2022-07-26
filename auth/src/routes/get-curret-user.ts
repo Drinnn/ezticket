8f1af0df4d291bd5b0c6c9faccd80e333027ead0
Drinnn <pedro_lontro@hotmail.com>
2022-07-26 09:03:35 -0300

    chore(auth): make middleware to get current user

--- auth/src/routes/get-curret-user.ts
+++ auth/src/routes/get-curret-user.ts
@@ -1,19 +1,10 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = Router();
 
-router.get("/api/auth", (req, res) => {
-  if (!req.session || !req.session.jwt) {
-    return res.status(200).send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_SECRET!);
-    res.status(200).send({ currentUser: payload });
-  } catch (err) {
-    res.status(401).send({ currentUser: null });
-  }
+router.get("/api/auth", currentUser, (req, res) => {
+  return res.status(200).send({ currentUser: req.currentUser || null });
 });
 
 export default router;
